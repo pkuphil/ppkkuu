dfe724365b5ba12414eda2c2cc3a1b158e2cf844
pkuphil <125347497+pkuphil@users.noreply.github.com>
2025-12-20 13:25:04 +0000

    Deploying to gh-pages from @ pkuphil/ppkkuu@28a90dc7d9ef4854bc352eef40435412d06050ef 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-talks",
           title: "talks",
-          description: "Talks and presentations.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/ppkkuu/talks/";
