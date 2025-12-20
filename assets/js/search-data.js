f56e7adbb24fb4f4611c8356a4f165c5caa59a9b
pkuphil <125347497+pkuphil@users.noreply.github.com>
2025-12-20 13:22:21 +0000

    Deploying to gh-pages from @ pkuphil/ppkkuu@0f30042f2f1a9ab1e64238f77cbd1bc6b1bc5aff 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "",
+          description: "A list of my work.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/ppkkuu/publications/";
